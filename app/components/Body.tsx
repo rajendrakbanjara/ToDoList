332cfcef5bf3b62e8710d27fdb72381e4d4d4c56
Rajendra Kumar <rajendrakbanjara@gmail.com>
2024-01-04 14:47:09 +0530

    list checked and marking functioning done.

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -16,32 +16,32 @@ export default function Body() {
     });
   };
 
-   // function for delete button
-   const deleteItems = (id:any) =>{
-    setItems((oldItems)=>{
-      return oldItems.filter((arrElem, index)=>{
+  // function for delete button
+  const deleteItems = (id: any) => {
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
         return index !== id;
       });
     });
-  }
+  };
   return (
     // main body
     <div className="bg-purple-900">
       {/*Main Card div */}
       <div className="flex items-center justify-center h-screen">
         {/* card content */}
-        <div className="bg-white min-w-max p-5 rounded-xl h-2/3">
+        <div className="bg-white min-w-max p-5 rounded-3xl h-2/3">
           {/* input elements */}
           <div className="flex">
             <input
               type="text"
               placeholder="Create your task here"
               onChange={itemEvent}
-              className="border-s-gray-500"
+              className="border border-slate-300 mr-3 p-3"
             />
             <button
               type="button"
-              className="bg-purple-900 text-white font-semibold px-5 py-2 rounded-[3px]"
+              className="bg-purple-900 text-white font-semibold px-7 py-2 rounded-[3px]"
               onClick={listOfItem}
             >
               add
@@ -49,10 +49,17 @@ export default function Body() {
           </div>
           <hr className="border-stone-300 mt-6" />
           {/* add task */}
-          <div className="max-h-full">
+          <div className="max-h-full ">
             <ol>
               {Items.map((itemVal, index) => {
-                return <TodoList key={index} id={index} text={itemVal} onSelect={deleteItems} />;
+                return (
+                  <TodoList
+                    key={index}
+                    id={index}
+                    text={itemVal}
+                    onSelect={deleteItems}
+                  />
+                );
               })}
             </ol>
           </div>
--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,21 +1,41 @@
 import React from "react";
+import { useState } from "react";
 import { RiDeleteBin6Fill } from "react-icons/ri";
+
+
 const TodoList = (props: any) => {
 
- 
-
+  //useState for mark the line
+    const [isChecked, setIsChecked] = useState(false);
+  
+    const handleCheckboxChange = () => {
+      setIsChecked(!isChecked);
+    };
+  
   return (
     <>
-      <div className="flex mt-4 mb-4 ">
-        <input type="radio" className="size-6 mr-5" />
-        <li className="">{props.text}</li>
-        <div 
-          className="ml-auto bg-slate-300 rounded-full p-2"
-          onClick={()=>{
+      <div className="flex items-center mt-4 mb-4 pl-2 pr-2">
+        {/* <input type="radio" className=" size-6 mr-5 text-green-500" /> */}
+
+        <input
+          type="checkbox"
+          className="form-checkbox my-4 h-6 w-6 rounded-full p-3 text-green-700 checked:bg-green-700 mr-4"
+          name="radio"
+          value="1"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+
+        <li className={isChecked ? 'line-through' : ''}>{props.text}</li>
+        <div
+          className="ml-auto flex items-center"
+          onClick={() => {
             props.onSelect(props.id);
           }}
-          >
-          <RiDeleteBin6Fill />
+        >
+          <p className="bg-slate-400 p-2 rounded-full">
+            <RiDeleteBin6Fill />
+          </p>
         </div>
       </div>
       <hr className="border-stone-300" />
