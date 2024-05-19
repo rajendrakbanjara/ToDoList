7f219fd8bb3c298a2f2f3cf0940f4bf8c383a47c
Rajendra Kumar <rajendrakbanjara@gmail.com>
2024-05-19 13:40:44 +0530

    initial commit

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -1,9 +1,9 @@
-"use client";
+"use client"
 import React, { useState } from "react";
 import TodoList from "./TodoList";
+
 export default function Body() {
   const [inputList, setInputList] = useState("");
-  // const [Items, setItems] = useState([]);
   const [Items, setItems] = useState<string[]>([]);
 
   const itemEvent = (event: any) => {
@@ -11,58 +11,49 @@ export default function Body() {
   };
 
   const listOfItem = () => {
-    setItems((oldItems) => {
-      return [...oldItems, inputList];
-    });
+    setItems((oldItems) => [...oldItems, inputList]);
+    setInputList("");
   };
 
-  // function for delete button
   const deleteItems = (id: any) => {
-    setItems((oldItems) => {
-      return oldItems.filter((arrElem, index) => {
-        return index !== id;
-      });
-    });
+    setItems((oldItems) => oldItems.filter((_, index) => index !== id));
   };
+
   return (
-    // main body
-    <div className="bg-purple-900">
-      {/*Main Card div */}
-      <div className="flex items-center justify-center h-screen">
-        {/* card content */}
-        <div className="bg-white min-w-max p-5 rounded-3xl h-2/3">
-          {/* input elements */}
-          <div className="flex">
-            <input
-              type="text"
-              placeholder="Create your task here"
-              onChange={itemEvent}
-              className="border border-slate-300 mr-3 p-3"
-            />
-            <button
-              type="button"
-              className="bg-purple-900 text-white font-semibold px-7 py-2 rounded-[3px]"
-              onClick={listOfItem}
-            >
-              add
-            </button>
-          </div>
-          <hr className="border-stone-300 mt-6" />
-          {/* add task */}
-          <div className="max-h-full ">
-            <ol>
-              {Items.map((itemVal, index) => {
-                return (
-                  <TodoList
-                    key={index}
-                    id={index}
-                    text={itemVal}
-                    onSelect={deleteItems}
-                  />
-                );
-              })}
-            </ol>
-          </div>
+    <div className="bg-purple-900 min-h-screen flex flex-col items-center justify-center">
+      <div className="ToDoListHeading font-sans font-semibold bg-white rounded-lg p-3 mb-4 text-xl">Make your To-Do</div>
+      {/* Card section */}
+      <div className="bg-white p-5 rounded-3xl w-full sm:w-2/3 lg:w-1/3  h-[34rem]">
+        {/* Input field */}
+        <div className="flex flex-col sm:flex-row mb-4">
+          <input
+            type="text"
+            placeholder="Create your task here"
+            value={inputList}
+            onChange={itemEvent}
+            className="border border-slate-300 p-3 mb-4 sm:mb-0 sm:mr-3 flex-grow"
+          />
+          <button
+            type="button"
+            className="bg-purple-900 text-white font-semibold px-7 py-2 rounded-md"
+            onClick={listOfItem}
+          >
+            Add
+          </button>
+        </div>
+        <hr className="border-stone-300 mb-4" />
+        {/* show data field */}
+        <div className="overflow-auto max-h-96">
+          <ol>
+            {Items.map((itemVal, index) => (
+              <TodoList
+                key={index}
+                id={index}
+                text={itemVal}
+                onSelect={deleteItems}
+              />
+            ))}
+          </ol>
         </div>
       </div>
     </div>
